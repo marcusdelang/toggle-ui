3b420d783ae6a443382c3b889397b8a44fd71b91
Jinlei2018 <43339636+Jinlei2018@users.noreply.github.com>
2019-04-30 17:11:50 +0200

    change to mvc

--- calendar.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
- * Shows the Calendar page view.
- */
-require_once './resources/fragments/start.php';
-include TASTY_RECIPES_VIEWS . 'calendarView.php';
--- meatballs.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
- * Shows the Pancakes page view.
- */
-require_once './resources/fragments/start.php';
-include TASTY_RECIPES_VIEWS . 'meatballsView.php';
