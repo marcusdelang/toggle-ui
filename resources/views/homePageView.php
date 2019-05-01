8a30d4ecc34e1fb3a9ad3a2b33693aedcf1cb076
Jinlei2018 <43339636+Jinlei2018@users.noreply.github.com>
2019-05-01 10:27:00 +0200

    onAndOffButtonsOnHomePage

---
+++ resources/views/homePageView.php
@@ -0,0 +1,17 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+	<title>On and off</title>
+    <script type="text/javascript" src="/resources/js/onAndOff.js"></script>
+	</head>
+	<body>
+	<div>
+		<form id="onOffForm" action="" method="POST">
+			<input id="on" type="button" value="TurnOn" name="onButton">
+			<br><br>		
+			<input id="off" type="button" value="TurnOff" name="offButton">			
+		</form>
+	</div>
+	</body>
+</html>
