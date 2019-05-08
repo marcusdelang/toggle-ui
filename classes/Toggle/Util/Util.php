bf3a6e74ef9ea1534b52ff628d1c1d06b5ed4e88
Michel Frog <m.quadria@gmail.com>
2019-05-08 09:35:37 +0200

    added util class and change structure

---
+++ classes/Toggle/Util/Util.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Chat\Util;
+
+/**
+ * Some utility methods.
+ */
+final class Util {
+
+    const SYMBOL_PREFIX = "TOGGLE_";
+
+    private function __construct() {
+        
+    }
+
+    /**
+     * This function should be called first in any PHP page receiving a HTTP request.
+     */
+    public static function initRequest() {
+        spl_autoload_register(function ($class) {
+            require_once 'classes/' . \str_replace('\\', '/', $class) . '.php';
+        });
+
+        session_start();
+        self::defineConstants();
+    }
+
+    private static function defineConstants() {
+        self::defineConstant('VIEWS', 'resources/views/');
+        self::defineConstant('CSS', 'resources/css/');
+        self::defineConstant('FRAGMENTS', 'resources/fragments/');
+    }
+
+    private static function defineConstant($param, $value) {
+        define(self::SYMBOL_PREFIX . $param, $value);
+    }
+
+}
